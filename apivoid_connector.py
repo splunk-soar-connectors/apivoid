6ca3653b7494ab358ddedb258d9af4baf0cc8c58
ngaglani <ngaglani@splunk.com>
2021-01-12 19:22:38 +0530

    Updating the latest_tested_versions in JSON

--- apivoid_connector.py
+++ apivoid_connector.py
@@ -63,7 +63,7 @@ class ApivoidConnector(BaseConnector):
             # Remove the script, style, footer and navigation part from the HTML message
             for element in soup(["script", "style", "footer", "nav"]):
                 element.extract()
-            error_text = soup.text.encode('utf-8')
+            error_text = soup.text
             split_lines = error_text.split('\n')
             split_lines = [x.strip() for x in split_lines if x.strip()]
             error_text = '\n'.join(split_lines)
